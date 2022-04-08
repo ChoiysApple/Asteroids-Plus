1adb8de4c31fb89ab118903340b7436c89eb6580
ChoiysApple <chcs1370@gmail.com>
2022-04-08 14:08:17 +0900

    Init Frame

--- Asteroid.swiftpm/GameScene.swift
+++ Asteroid.swiftpm/GameScene.swift
@@ -8,9 +8,26 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
     override func didMove(to view: SKView) {
-        print("You are in the game scene!")
-        backgroundColor = .red
+        
+        backgroundColor = .black
+        
+        let path = CGMutablePath()
+        path.move(to: .zero)
+        path.addLine(to: CGPoint(x: 200, y: 50))
+        path.addLine(to: CGPoint(x: 150, y: 150))
+        path.addLine(to: CGPoint(x: 200, y: 50))
+        path.addLine(to: .zero)
+        
+        let shape = SKShapeNode(path: path)
+        shape.strokeColor = .white
+        shape.lineWidth = 1
+        shape.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        self.addChild(shape)
+        
+        
     }
+    
+    
 }
-
