8c1069d985e51a5b4800879d0472df331b4c11a4
ChoiysApple <chcs1370@gmail.com>
2022-04-11 15:01:32 +0900

    Make Ship follow finger
    Update GameScene.swift

--- Asteroid.swiftpm/GameScene.swift
+++ Asteroid.swiftpm/GameScene.swift
@@ -18,12 +18,28 @@ class GameScene: SKScene {
                 
     }
     
+    override func update(_ currentTime: TimeInterval) {
+                
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+        if let point = touches.first?.location(in: self) {
+            orientShip(point: point)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let point = touches.first?.location(in: self) {
+            orientShip(point: point)
+        }
+    }
+    
+    private func orientShip(point: CGPoint) {
         let ship = childNode(withName: kShipName)
-        guard let touchLocation = touches.first?.location(in: self) else { return }
 
-        let lookAtConstraint = SKConstraint.orient(to: touchLocation, offset: SKRange(constantValue: -CGFloat.pi / 2))
+        let lookAtConstraint = SKConstraint.orient(to: point, offset: SKRange(constantValue: -CGFloat.pi / 2))
         ship?.constraints = [ lookAtConstraint ]
     }
     
